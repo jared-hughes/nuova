45cab9f88e954e2f563ab60b51e11a3a5ce0b6a3
Jared Hughes <jahughes241@gmail.com>
2022-09-26 00:26:04 -0700

    Remove C solver

--- Makefile
+++ Makefile
@@ -2,14 +2,14 @@ pipe: nuova solver
 	./solver
 	./nuova prog
 
+run-raw-nuova: raw-nuova
+	./raw-nuova prog
+
+raw-nuova:
+	gcc raw-nuova.c -o raw-nuova
+
 run-nuova: nuova
 	./nuova prog
 
 nuova: nuova.c
 	gcc nuova.c -o nuova
-
-run-solver: solver
-	./solver
-
-solver: solver.c
-	gcc solver.c -o solver
--- solver.py
+++ solver.py
@@ -6,6 +6,21 @@ from z3 import *
 u = lambda x: BitVecVal(x, 32)
 
 def P(x):
+  if type(x) != int:
+    raise TypeError("P argument should be int. If you want z3, use P_z3")
+  x ^= x >> 17
+  x *= 0xed5ad4bb
+  x &= 0xFFFFFFFF
+  x ^= x >> 11
+  x *= 0xac4c1b51
+  x &= 0xFFFFFFFF
+  x ^= x >> 15
+  x *= 0x31848bab
+  x &= 0xFFFFFFFF
+  x ^= x >> 14
+  return x
+
+def P_z3(x):
   x = x ^ LShR(x, 17)
   x = x * u(0xed5ad4bb)
   x = x ^ LShR(x, 11)
@@ -14,9 +29,6 @@ def P(x):
   x = x * u(0x31848bab)
   x = x ^ LShR(x, 14)
   return x
-
-def inverseP(y):
-  return sol(X, P(X) == y)
 
 X = BitVec("X", 32)
 Y = BitVec("Y", 32)
@@ -27,32 +39,43 @@ def sol(conds):
   s.check()
   return s.model().eval(X)
 
-# print(sol(P(X)==u(0x01)))
-
-def load(x):
-  a = x ^ P(Y)
-  return P(a)
-
-def const_output(string: bytes, start: int):
+def const_output(string: bytes):
+  global idx
   for c in string:
-    y = start
     while True:
       s = Solver()
-      s.add(P(X ^ P(u(y + 2))) & u(0xFF) == u(c))
+      s.add(P_z3(X ^ u(P(idx + 2))) & u(0xFF) == u(c))
       s.add(X >> 8 == 0)
       if s.check().r == Z3_L_TRUE:
-        print("put1(0x00);\n" * (y - start))
-        print()
-        print(f"// putchar({c}) // => {repr(chr(c))}")
-        print(f"put1(0xFF);")
-        print(f"put4(0x0F ^ P({y + 1}));")
-        print(f"put1({s.model().eval(X)});")
-        print(f"put1(0xFF);")
-        print(f"put4(0xD0 ^ P({y + 4}));")
-        start = y + 5
+        # print(f"// putchar({c}) // => {repr(chr(c))}")
+        put1(0xFF)
+        put4(0x0F ^ P(idx))
+        put1(s.model().eval(X).as_long())
+        put1(0xFF)
+        put4(0xD0 ^ P(idx))
         break
-      y += 1
+      put1(0x00)
 
-# const_output(b"Hi", 0)
+def exit():
+  global idx
+  put1(0xFF)
+  put4(0xF0 ^ P(idx))
 
-# const_output(b"Hello, World!\n", 0)
+def put1(x):
+  global prog, idx
+  prog.write(bytes([x]))
+  idx += 1
+
+def put4(x):
+  global prog, idx
+  prog.write(bytes([x >> 24, x >> 16 & 0xFF, x >> 8 & 0xFF, x & 0xFF]))
+  idx += 1
+
+try:
+  prog = open("prog", "wb")
+  idx = 0
+  const_output(b"Hi")
+  # const_output(b"Hello, World!\n")
+  exit()
+finally:
+  prog.close()
